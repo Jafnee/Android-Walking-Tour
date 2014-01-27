a9e38cfb7a7c5a7e92375ff69420fe3b7ce8a1ae
Owen Day <owen93day@hotmail.co.uk>
2014-01-27 11:17:06 +0000

    Added jUnit tests

---
+++ SRC/Drafts/0.3/fuzzyNinja/PathFinder/src/uk/ac/aber/imaps/cs22120/fuzzyNinja/pathfinder/model/TestWaypoint.java
@@ -0,0 +1,48 @@
+package uk.ac.aber.imaps.cs22120.fuzzyNinja.pathfinder.model;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestWaypoint{
+
+	@test
+	public void testGetCoordinatesWithValidValue(){
+	
+	}
+	
+	@test
+	public void testGetCoordinatesWithNullValue(){
+	
+	}
+	
+	@test
+	public void testGetNameWithValidValue(){
+	
+	}
+	
+	@test
+	public void testGetNameWithNullValue(){
+	
+	}
+	
+	@test
+	public void testGetDescriptionWithValidValue(){
+	
+	}
+	
+	@test
+	public void testGetDescriptionWithNullValue(){
+	
+	}
+	
+	@test
+	public void testGetTimestampWithValidValue(){
+	
+	}
+	
+	@test
+	public void testGetTimeStampWithNullValue(){
+	
+	}
+
+}
