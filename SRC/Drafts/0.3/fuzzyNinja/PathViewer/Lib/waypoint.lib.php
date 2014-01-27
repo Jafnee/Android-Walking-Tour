6f5b2d40ddef0b1072e11b2b899147ed968feb13
Lewis Dale <lpd1@aber.ac.uk>
2014-01-27 12:02:55 +0000

    Draft of web code updated::

--- SRC/Drafts/0.3/fuzzyNinja/PathViewer/Lib/waypoint.lib.php
+++ SRC/Drafts/0.3/fuzzyNinja/PathViewer/Lib/waypoint.lib.php
@@ -1,4 +1,72 @@
 <?php
 class waypoint {
+    public function __construct() {}
+    
+    /*Populates the object from an array*/
+    public function __populate($data) 
+    {
+        if(!is_array($data)) return NULL;
+        foreach($data as $key => $val)
+        {
+            $this->$key = $val;
+        }
+    }
+    
+    public function load($id)
+    {
+        if(!$id) return NULL;
+        
+        global $db;
+        $query = "SELECT id,walkID,latitude,longitude,timestamp FROM locations
+                    WHERE
+                    id = '".$db->real_escape_string($id)."'
+                    LIMIT 1";
+        $result = $db->query( $query );
+        $row = $result->fetch_assoc();
+        $this->__populate($row);
+    }
+    
+    
+    public function load_walk($walkID)
+    {
+        if(!$walkID) return NULL;
+        global $db;
+        $walks=array();
+        
+        $query = "SELECT id,walkID,latitude,longitude,timestamp FROM locations
+                    WHERE
+                    walkID='".$db->real_escape_string($walkID)."'";
+        
+        $result=$db->query( $query );
+        while($row=$result->fetch_assoc())
+        {
+            $hold=new waypoint();
+            $hold->__populate($row);
+            $walks[]=$hold;
+        }
+        
+        return $walks;
+    }
+    
+    public function get_latitude()
+    {
+        return $this->latitude;
+    }
+    
+    public function get_longitude()
+    {
+        return $this->longitude;
+    }
+    
+    /*Save the waypoint*/
+    public function save($data) {
+        
+    }
+    
+    /*Transfer the image*/
+    public function transfer_image()
+    {
+        
+    }
 }
 ?>
--- SRC/Drafts/0.3/fuzzyNinja/PathViewer/index.php
+++ SRC/Drafts/0.3/fuzzyNinja/PathViewer/index.php
@@ -1,6 +1,36 @@
 <?php
-/*SWITCH ON 'M' VARIABLE e.g. /index.php?m=view_tour*/
-switch($_GET['m'])  {
-	default: break;
+include($_SERVER['DOCUMENT_ROOT']."/Lib/config.lib.php");
+
+$cmd = $_GET['m'];
+
+switch($cmd)
+{
+    case "view":
+        /*View a tour*/
+        $id=$_GET['id'];
+        if(!$id) { header("Location: /index.php?m=404"); }
+        
+        $route->load($id);
+        print_r($route);
+        break;
+    case "list": 
+        /*Lists all tours*/
+        break;
+    case "upld": 
+        /*Sends a response of true if the POST is received, or false if nothing received*/
+        header("Content-Type:plain/text");
+        $data = $_POST;
+        if($data) {
+            echo "true";
+        } else {
+            echo "false";
+        }
+        break;
+    case "404":
+        $string = "<h1>OOPS</h1><h2>The Page Could Not Be Found</h2>";
+        echo $string;
+        break;
+    default: break;
 }
+exit;
 ?>
