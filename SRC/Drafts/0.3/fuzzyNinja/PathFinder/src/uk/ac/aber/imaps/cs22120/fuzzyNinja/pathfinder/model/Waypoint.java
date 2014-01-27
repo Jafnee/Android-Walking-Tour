cb618b2c9e1a675778eae2e192d8bec91ea9d3b4
Jafnee <jafneejesmee@gmail.com>
2014-01-27 10:43:31 +0000

    attempt to fix #1

--- SRC/Drafts/0.3/fuzzyNinja/PathFinder/src/uk/ac/aber/imaps/cs22120/fuzzyNinja/pathfinder/model/Waypoint.java
+++ SRC/Drafts/0.3/fuzzyNinja/PathFinder/src/uk/ac/aber/imaps/cs22120/fuzzyNinja/pathfinder/model/Waypoint.java
@@ -1,30 +1,59 @@
-/* 
- * Author: Joshua C Moss
- * Date Created:
- * Date Last Modified:
- * Co Authors:
- * 
- * 
- * */
-
 /*
- * Scope: This class is an extension of a RouteLocation; a location on the route,
- * holding information about the current point as opposed to the position only.
- * 
+ * Scope: This class is a extension of a location on the route, holding information about the current point.
  * */
-
-
 
 package uk.ac.aber.imaps.cs22120.fuzzyNinja.pathfinder.model;
 
 public class Waypoint extends RouteLocation 
 {
-
+	private Location coordinates;
+	private String name;
+	private String description;
+	//photos
+	private Date timestamp;
+	
 	public Waypoint()
-	{
-		
-		
+	{		
 		
 	}
 	
+	public Location getCoordinates() {
+		//get coords
+	}
+	
+	public void setCoordinates(double lat, double lng) {
+		//sets coords
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String n) {
+		name = n;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String d) {
+		description = d;
+	}
+	
+//	public PICTUREDATATYPE getPhoto() {
+//		//return photo
+//	}
+	
+//	public void setPhoto(PICTUREDATATYPE p) {
+//		photos = p;
+//	}
+	
+	public Date getTimestamp() {
+		return timestamp;
+	}
+	
+	public void setTimestamp(Date t) {
+	timestamp = t;
+	}
 }
