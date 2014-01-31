5b3b582d0b12f6bd9d378faf90b08a001e2058e4
Jafnee <jafneejesmee@gmail.com>
2014-01-31 06:47:42 +0000

    Added Javadoc for: - Route - Waypoint
    To do:
    -@author
    -@since
    -@version
    -hour and distance in Route
    
     Method for hours and distance not commented. Authors need to be added. File header checked needs to be checked (version n copyright)

--- SRC/Drafts/0.5/App/Pathfinder/src/main/java/cs221/group15/pathfinder/Route.java
+++ SRC/Drafts/0.5/App/Pathfinder/src/main/java/cs221/group15/pathfinder/Route.java
@@ -1,12 +1,21 @@
-package cs221.group15.pathfinder;
-
 /*
  *@(#)Route.java 0.5 2014-01-31
  * 
  * Copyright (c)2014 Aberystwyth University.
  * All rights reserved.
  */
+package cs221.group15.pathfinder;
 
+/**
+ * The Route class will represent a Walking Tour.
+ * The route will consists of multiple waypoints.
+ * The waypoints will be stored in an array list of Waypoint objects {@link #waypoint}.
+ * @see Waypoint
+ * 
+ * @author
+ * @since
+ * @version
+ */
 public class Route {
     private long id;
     private String shortDescription;
@@ -19,8 +28,8 @@ public class Route {
     
     
 	/**
-	 * This class constructor will create a walking tour and fill it in with the title, short description
-	 * and long description the user has specified.
+	 * This class constructor will create a walking tour and fill it in with the title,
+	 * short description and long description the user has specified. 
 	 * 
 	 * @param title the name given to the walking tour
 	 * @param shortDescription the short description of the tour, limited to 100 characters
@@ -34,7 +43,7 @@ public class Route {
     
     
 	/**
-	 * Sets the ID number of the walking tour
+	 * Sets the ID number of the walking tour.
 	 * 
 	 * @param id is a unique number for the walking tour, allowing it to be identified
 	 */
@@ -55,7 +64,7 @@ public class Route {
     
     
 	/**
-	 * Will assign the walking tour with a new short description
+	 * Will assign the walking tour with a new short description.
 	 * 
 	 * @param shortDescription
 	 */
@@ -64,7 +73,7 @@ public class Route {
     }
 
     /**
-     * Returns the short description of the walking tour
+     * Returns the short description of the walking tour.
      * 
      * @return shortDescription as a string
      */
@@ -74,7 +83,7 @@ public class Route {
 
     
     /**
-     * Will assign the walking tour with a new long description
+     * Will assign the walking tour with a new long description.
      * 
      * @param longDescription
      */
@@ -84,7 +93,7 @@ public class Route {
 
     
     /**
-     * Returns the long description of the walking tour
+     * Returns the long description of the walking tour.
      * 
      * @return longDescription as a string
      */
@@ -101,31 +110,84 @@ public class Route {
         this.hours = hours;
     }
 
+    
+    /**
+     * 
+     * @return
+     */
     public float getHours() {
         return this.hours;
     }
 
+    
+    /**
+     * 
+     * @param distance
+     */
     public void setDistance(float distance) {
         this.distance = distance;
     }
 
+    
+    /**
+     * 
+     * @return
+     */
     public float getDistance() {
         return this.distance;
     }
+    
+    
+    /**
+     * This will set waypoint {@link Route#waypoint} to the specified
+     * array of Waypoint objects.
+     * @see Waypoint
+     * 
+     * @param waypoints the array of Waypoint objects
+     */
     public void setWaypoints(Waypoint[] waypoints) {
         this.waypoint = waypoints;
     }
 
+    
+    /**
+     * Will set the walking tour's title.
+     * 
+     * @param title is the specified String
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    
+    /**
+     * Returns the title of the walking tour.
+     * 
+     * @return the title as a String
+     */
     public String getTitle() {
         return this.title;
     }
 
-    public int getTotalWaypoints() { return this.waypoint.length; }
+    
+    /**
+     * Will return the number of waypoints in the walking tour.
+     * 
+     * @return the number of waypoints as an int value
+     */
+    public int getTotalWaypoints() {
+    	return this.waypoint.length; 
+    }
 
-    public Waypoint getWaypoint(int i) { return this.waypoint[i]; }
+    
+    /**
+     * Will return a specified waypoint from the array.
+     * 
+     * @param i the index of the waypoint in the array
+     * @return an object of Waypoint
+     */
+    public Waypoint getWaypoint(int i) {
+    	return this.waypoint[i]; 
+    }
 
 }
--- SRC/Drafts/0.5/App/Pathfinder/src/main/java/cs221/group15/pathfinder/Waypoint.java
+++ SRC/Drafts/0.5/App/Pathfinder/src/main/java/cs221/group15/pathfinder/Waypoint.java
@@ -1,9 +1,31 @@
+/*
+ *@(#)Waypoint.java 0.5 2014-01-31
+ * 
+ * Copyright (c)2014 Aberystwyth University.
+ * All rights reserved.
+ */
 package cs221.group15.pathfinder;
 
-/**
- * Created by gad16 on 29/01/2014.
- */
 
+/**
+ * This class is for the individual waypoints that will get created
+ * along the walking tour. Each waypoint object will have GPS coordinates
+ * and a timestamp.The object will also have a description and a picture 
+ * provided by the user.
+ * 
+ * The latitude and longitude coordinates will be used on the website
+ * to place a marker of the waypoint's location on the map.
+ * @see LocationListen#onLocationChanged(android.location.Location)
+ * 
+ * The waypoints will be added to a Route's array, therefore becoming part
+ * of that walking tour.
+ * @see Route
+ * 
+ * 
+ * @author 
+ * @since
+ * @version
+ */
 public class Waypoint {
     private float lat;
     private float lng;
@@ -14,66 +36,169 @@ public class Waypoint {
     private String[] photos;
     private String description;
 
+    
+    /**
+     * The constructor will create the Waypoint object and will pass in
+     * the walkID which is the ID of the Route(walking tour) it belongs to.
+     * 
+     * @param id 		the unique number to identify this waypoint
+     * @param walkID 	the ID number of the Route it belongs to
+     */
     Waypoint(long id, long walkID) {
         this.id = id;
         this.walkID = walkID;
     }
 
+    
+    /**
+     * This class constructor will only create the waypoint object,
+     * it will not assign it to a Route.
+     * 
+     * @param walkID the unique number to identify this waypoint
+     */
     Waypoint(long walkID) {
         this.walkID = walkID;
     }
+    
+    
+    /**
+     * Returns the latitude coordinate of the waypoint.
+     * 
+     * @return the float value of latitude
+     */
     public float getLat() {
         return lat;
     }
 
+    
+    /**
+     * Sets latitude value of the waypoint.
+     * 
+     * @param lat a float value of latitude
+     */
     public void setLat(float lat) {
         this.lat = lat;
     }
 
+    
+    /**
+     * Returns the longitude coordinate of the waypoint.
+     * 
+     * @return the float value of longitude
+     */
     public float getLng() {
         return lng;
     }
 
+    
+    /**
+     * Sets the longitude value of the waypoint.
+     * 
+     * @param lng a float value of longitude
+     */
     public void setLng(float lng) {
         this.lng = lng;
     }
+    
 
+    /**
+     * Returns the ID of the walking tour it belongs to.
+     * 
+     * @return the ID of the route it belongs to as a long
+     */
     public long getWalkID() {
         return walkID;
     }
 
+    
+    /**
+     * Sets the ID of the walking tour the waypoint belongs to.
+     * 
+     * @param walkID an integer value of the Route ID
+     */
     public void setWalkID(int walkID) {
         this.walkID = walkID;
     }
 
+    
+    /**
+     * Returns the ID of the waypoint.
+     * 
+     * @return the ID of the waypoint as a long
+     */
     public long getId() {
         return id;
     }
 
+    
+    /**
+     * Sets the ID of the waypoint.
+     * 
+     * @param id an integer value
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    
+    /**
+     * Returns the timestamp of the location, will include the date and time.
+     * 
+     * @return the timestamp as a float
+     */
     public float getTimestamp() {
         return timestamp;
     }
 
+    
+    /**
+     * Sets the timestamp of the waypoint, will set the date and time
+     * the waypoint was created.
+     * 
+     * @param timestamp a float value representing date and time
+     */
     public void setTimestamp(float timestamp) {
         this.timestamp = timestamp;
     }
 
+    
+    /**
+     * Returns the photo as a string, this allows the picture to be
+     * uploaded to the webserver via a HTTP Post.
+     * @see HTTPPostSender
+     * 
+     * @return a String representation of the photo
+     */
     public String[] getPhotos() {
         return photos;
     }
 
+    
+    /**
+     * Will assign a photo to the waypoint.
+     * 
+     * @param photo as a string representation
+     */
     public void setPhoto(String[] photo) {
         this.photos = photo;
     }
 
+    
+    /**
+     * Returns the description of the waypoint.
+     * 
+     * @return as a String
+     */
     public String getDescription() {
         return description;
     }
 
+    
+    /**
+     * Sets the waypoint with a specified description.
+     * 
+     * @param description is a String the user specifies.
+     */
     public void setDescription(String description) {
         this.description = description;
     }
