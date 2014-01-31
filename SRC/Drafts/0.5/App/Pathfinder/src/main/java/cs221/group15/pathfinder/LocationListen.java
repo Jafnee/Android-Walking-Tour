f6728ddbf747e55119b4239a93447513911ce635
Jafnee <jafneejesmee@gmail.com>
2014-01-31 09:51:39 +0000

    Added Javadoc for: - Route                        (complete : not checked) - Waypoint                 (complete : not checked) -LocationListen         (complete : not checked) -DatabaseHandler
    To do:
    -@author
    -@since
    -@version
    -hour and distance in Route
    -Methods in LocationListen
    -get Javadoc for DatabaseHandler checked (last method getroutes() unsure )
    
     Method for hours and distance not commented. Authors need to be added. File header checked needs to be checked (version n copyright)

--- SRC/Drafts/0.5/App/Pathfinder/src/main/java/cs221/group15/pathfinder/LocationListen.java
+++ SRC/Drafts/0.5/App/Pathfinder/src/main/java/cs221/group15/pathfinder/LocationListen.java
@@ -13,21 +13,21 @@ import android.os.Bundle;
 /**
  * This class handles the GPS coordinates that will be assigned to
  * the waypoints. The class will have an object of type Location
- * that will update itself when the user moves to a new position.
+ * that will update itself when the user moves to a new position,
+ * the GPS coordinates will then be acquired from these positions.
  * @see Waypoint
  * 
  * 
  * 
- * @author
- * @since
- * @version
+ * @author 	lpd1
+ * @since 	0.1
+ * @version 1.0 2014-01-31 9ed077e148cf94320295b5f4553fb380cb6c0c43
  */
 public class LocationListen implements LocationListener {
     MainActivity parent;
 
     LocationListen(MainActivity parent) {
         this.parent=parent;
-
     }
 
     
@@ -43,16 +43,28 @@ public class LocationListen implements LocationListener {
         parent.show(location.getLatitude(),location.getLongitude());
     }
 
+    
+    /**
+     * Called when the provider status changes.
+     */
     @Override
     public void onStatusChanged(String s, int i, Bundle bundle) {
 
     }
 
+    
+    /**
+     * Called when the provider is enabled by the user.
+     */
     @Override
     public void onProviderEnabled(String s) {
 
     }
 
+    
+    /**
+     * Called when the provider is disabled by the user.
+     */
     @Override
     public void onProviderDisabled(String s) {
 
